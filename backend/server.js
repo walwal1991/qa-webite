5ac0c642bb295035f7150f7b686830e701e777b3
walwal1991 <89856662+walwal1991@users.noreply.github.com>
2025-12-23 10:24:08 -0500

    Update server2.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const searchRoutes = require('./routes/search');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'qa-webite3.vercel.app', // React frontend
+  origin: 'https://qa-webite-4.vercel.app/', // React frontend
   credentials: true
 }));
 
